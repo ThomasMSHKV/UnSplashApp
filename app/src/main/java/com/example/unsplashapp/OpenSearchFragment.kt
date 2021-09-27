53d49970461c237fa6066184807130c3eff2ddc1
OlegDeveloper <tmereshkov@gmail.com>
2021-09-27 23:19:16 +0300

    Add JSON

---
+++ app/src/main/java/com/example/unsplashapp/OpenSearchFragment.kt
@@ -0,0 +1,20 @@
+package com.example.unsplashapp
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+
+
+class OpenSearchFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_open_search, container, false)
+
+    }
+}
---
+++ app/src/main/java/com/example/unsplashapp/data/MainObjectClass.kt
@@ -0,0 +1,3 @@
+package com.example.unsplashapp.data
+
+class MainObjectClass : ArrayList<MainObjectClassItem>()
