078fc0c8f9af7518a22a459a50f64c84bb7ee36e
OlegDeveloper <tmereshkov@gmail.com>
2021-10-05 16:32:02 +0300

    OpenPicturePresenter.kt 2

--- app/src/main/java/com/example/unsplashapp/presentor/view/OpenPicturesPresenter.kt
+++ app/src/main/java/com/example/unsplashapp/presentor/view/OpenPicturesPresenter.kt
@@ -1,4 +1,20 @@
 package com.example.unsplashapp.presentor.view
 
-class OpenPicturesPresenter {
+import com.example.unsplashapp.data.ApiRepository
+import com.example.unsplashapp.data.Result
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlin.coroutines.CoroutineContext
+
+class OpenPicturesPresenter(val view: OpenPictureContract.View): OpenPictureContract.Presenter, CoroutineScope {
+    override val coroutineContext: CoroutineContext = Dispatchers.Main
+
+    private val repository = ApiRepository()
+    lateinit var result: Result
+
+    override fun getPictures(id: Int) {
+
+    }
+
+
 }
