21c41aad0af956e8968b6cae8e841455515866f7
OlegDeveloper <tmereshkov@gmail.com>
2021-12-12 23:08:28 +0300

    working Parcelize

--- app/src/main/java/com/example/unsplashapp/data/Result.kt
+++ app/src/main/java/com/example/unsplashapp/data/Result.kt
@@ -2,69 +2,25 @@ package com.example.unsplashapp.data
 
 import android.os.Parcel
 import android.os.Parcelable
+import kotlinx.android.parcel.Parcelize
+import kotlinx.android.parcel.RawValue
 
+@Parcelize
 data class Result(
     val alt_description: String,
     val blur_hash: String,
-    val categories: List<Any>,
+    val categories:  @RawValue List<Any>,
     val color: String,
     val created_at: String,
-    val current_user_collections: List<Any>,
+    val current_user_collections: @RawValue List<Any>,
     val description: String,
     val height: Int,
     val id: String,
     val liked_by_user: Boolean,
     val likes: Int,
     val promoted_at: String,
-    val sponsorship: Any,
+    val sponsorship:  @RawValue  Any,
     val updated_at: String,
-    val urls: UrlsX,
+    val urls: @RawValue UrlsX,
     val width: Int
-):Parcelable {
-    constructor(parcel: Parcel) : this(
-        parcel.readValue(Int::class.java.classLoader) as? Int,
-        parcel.readString()!!,
-        parcel.readString()!!,
-        parcel.readString()!!,
-        parcel.readString()!!,
-        parcel.readString()!!,
-        parcel.readParcelable(UrlsX::class.java.classLoader)!!,
-        parcel.readInt(),
-        parcel.readString()!!,
-        parcel.readByte() != 0.toByte(),
-        parcel.readInt(),
-        parcel.readString()!!,
-        parcel.readString()!!,
-        parcel.readInt()
-    ) {
-    }
-
-    override fun writeToParcel(parcel: Parcel, flags: Int) {
-        parcel.writeString(alt_description)
-        parcel.writeString(blur_hash)
-        parcel.writeString(color)
-        parcel.writeString(created_at)
-        parcel.writeString(description)
-        parcel.writeInt(height)
-        parcel.writeString(id)
-        parcel.writeByte(if (liked_by_user) 1 else 0)
-        parcel.writeInt(likes)
-        parcel.writeString(promoted_at)
-        parcel.writeString(updated_at)
-        parcel.writeInt(width)
-    }
-
-    override fun describeContents(): Int {
-        return 0
-    }
-
-    companion object CREATOR : Parcelable.Creator<Result> {
-        override fun createFromParcel(parcel: Parcel): Result {
-            return Result(parcel)
-        }
-
-        override fun newArray(size: Int): Array<Result?> {
-            return arrayOfNulls(size)
-        }
-    }
-}
+):Parcelable
